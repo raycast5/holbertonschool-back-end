1045ada883e9438f4e98367d2a053fb7cea5626b
raycast5 <ray.j.colon@gmail.com>
2022-12-05 23:09:49 -0400

    added if __name__ file 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,29 +5,29 @@ import json
 import requests
 from sys import argv
 
+if __name__ == "__main__":
+    name_source = requests.get(f"https://jsonplaceholder.typicode.com/users")
+    employees = json.loads(name_source.text)
+    task_source = requests.get(f"https://jsonplaceholder.typicode.com/todos")
+    todos = json.loads(task_source.text)
 
-name_source = requests.get(f"https://jsonplaceholder.typicode.com/users")
-employees = json.loads(name_source.text)
-task_source = requests.get(f"https://jsonplaceholder.typicode.com/todos")
-todos = json.loads(task_source.text)
+    total_tasks = 0
+    tasks_comp = 0
+    user_id = int(argv[1])
+    user_name = ""
+    task_list = []
 
-total_tasks = 0
-tasks_comp = 0
-user_id = int(argv[1])
-user_name = ""
-task_list = []
+    for user in employees:
+        if user_id == user['id']:
+            user_name = user['name']
 
-for user in employees:
-    if user_id == user['id']:
-        user_name = user['name']
+    for tasks in todos:
+        if int(tasks['userId']) == user_id:
+            total_tasks += 1
+            if tasks["completed"] is True:
+                tasks_comp += 1
+                task_list.append(tasks["title"])
 
-for tasks in todos:
-    if int(tasks['userId']) == user_id:
-        total_tasks += 1
-        if tasks["completed"] is True:
-            tasks_comp += 1
-            task_list.append(tasks["title"])
-
-print(f"Employee {user_name} is done with tasks ({tasks_comp}/{total_tasks}):")
-for task in task_list:
-    print(f"\t {task}")
+    print(f"Employee {user_name} is done with tasks ({tasks_comp}/{total_tasks}):")
+    for task in task_list:
+        print(f"\t {task}")
